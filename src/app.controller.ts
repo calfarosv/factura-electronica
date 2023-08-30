df0096caf88072b366a3ed116988c202fa03fc72
calfarosv <calfaro@cel.gob.sv>
2023-08-30 09:58:16 -0600

    antes de crear documentos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,3 +1,41 @@
+
+import { Controller, Get, Request, Post, UseGuards, HttpStatus } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AppService } from './app.service';
+
+//+++ agregando oauth y jwt ++++
+//import { LocalAuthGuard } from './auth/local-auth.guard';
+//import { AuthService } from './auth/auth.service';
+
+//+++
+
+@ApiTags('SISDTE')
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('Autor')
+  getAutor(): string {
+    return this.appService.getElAutor();
+
+  }
+/*
+  //auth
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+  */
+
+}
+
+/*
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
@@ -10,3 +48,4 @@ export class AppController {
     return this.appService.getHello();
   }
 }
+*/
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,67 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+//import { Trp_Dpe_Entity } from './permiso/entities/trp_dpe_entity';
+//import { Trp_Pei_Entity } from './permiso/entities/trp_pei_entity';
+//import { PermisoModule } from './permiso/permiso.module';
+//import { UsersModule } from './users/users.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+//import { Trp_Emp_V_Entity } from './permiso/entities/trp_emp_v_entity';
+import { ConfigModule } from '@nestjs/config';
+//import { Trp_Emj_V_Entity } from './permiso/entities/trp_emj_empjef_v';
+
+@Module({
+  imports: [//AuthModule, UsersModule, //PermisoModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'oracle',
+      connectString: process.env.ORA_CONECTION,
+      port: parseInt(process.env.ORA_PORT),
+      username: process.env.ORA_USERNAME,
+      password: process.env.ORA_PASSWORD,
+      database: process.env.ORA_DATABASE,
+      logging: true,
+      schema: '',
+      //entities: [Trp_Pei_Entity, Trp_Dpe_Entity, Trp_Emp_V_Entity,Trp_Emj_V_Entity],
+    }),
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: {
+          host: process.env.HOST_MAIL,
+          port: parseInt(process.env.PORT_MAIL),
+          secure: Boolean(JSON.parse(process.env.SECURE_MAIL)),
+        },
+        defaults: {
+          from: '"nest-modules" <modules@nestjs.com>',
+        },
+
+
+        template: {
+          dir: process.cwd() + '/templates/', // __dirname + '/templates', //dir: process.cwd() + 'src/templates',
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+
+      }),
+    }),
+
+
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+
+export class AppModule { }
+
+
+/*
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -8,3 +71,4 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+*/
